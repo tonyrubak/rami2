62d6c51e4c5b5d38a0de5c993c286d75ee800ff1
Joe Peralta <joe@crimsonhexagon.us>
2020-06-15 17:54:14 -0400

    added aws dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                  [org.suskalo/discljord "0.2.8"]]
+                  [org.suskalo/discljord "0.2.8"]
+                  [com.cognitect.aws/api "0.8.456"]
+                  [com.cognitect.aws/endpoints "1.1.11.789"]
+                  [com.cognitect.aws/dynamodb "799.2.679.0"]]
   :main ^:skip-aot rami2.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
