edcfb8629cca7283bbeb5fe6d13a260d42c4442c
Joe Peralta <joe@crimsonhexagon.us>
2020-06-17 17:47:25 -0400

    reformatted

--- src/rami2/markov.clj
+++ src/rami2/markov.clj
@@ -1,14 +1,22 @@
 (ns rami2.markov
-    (:require [cognitect.aws.client.api :as aws]
-                [clojure.data.json :as json]))
+  (:require [cognitect.aws.client.api :as aws]
+            [clojure.data.json :as json]))
 
 (defn markov
-    [prefix state]
+  [prefix state]
+  (if (= prefix "communism")
+    "communism begins"
     (let [lambda (aws/client {:api :lambda})]
-        (.replaceAll (get (json/read-str
+      (.replaceAll
+        (get
+          (json/read-str
             (slurp
-                (:Payload
-                    (aws/invoke
-                        lambda
-                        {:op :Invoke
-                        :request {:FunctionName "markov" :LogType "None" :Payload (format "{ \"prefix\": \"%s\" }" prefix)}})))) "body") "\"" "")))
+              (:Payload
+                (aws/invoke
+                  lambda
+                  {:op :Invoke
+                  :request {:FunctionName "markov"
+                           :LogType "None"
+                           :Payload (format "{ \"prefix\": \"%s\" }"
+                                            prefix)}}))))
+          "body") "\"" ""))))
