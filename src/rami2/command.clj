204967c0c93e9b59a53bf3416607218d3205daef
Joe Peralta <joe@crimsonhexagon.us>
2020-06-30 13:33:03 -0400

    formatting

--- src/rami2/command.clj
+++ src/rami2/command.clj
@@ -24,34 +24,32 @@
 (defmethod invoke-command "list" [command state]
   {:type :content
    :value (str/join ", "
-                    (map #(-> % :tag :S) (:Items (storage/list-aka state))))})
+                    (map #(-> % :tag :S)
+                         (:Items (storage/list-aka state))))})
 
 (defmethod invoke-command "bing" [command state]
   {:type :embed
-  :value (let [args (:args command)
-               resp (search/get-search-response args state)
-               q (str/join " " args)]
-           {:title (format "Bing results for %s" q)
-           :type "link"
-           :description (get resp "snippet")
-           :url (get resp "url")
-           :fields [{:name "URL" :value (get resp "url")}]
-           :thumbnail {:url "https://1000logos.net/wp-content/uploads/2017/12/bing-emblem.jpg"}})})
+   :value (let [resp (search/get-search-response (:args command) state)]
+            {:title (format "Bing results for %s" (str/join " " args))
+             :type "link"
+             :description (get resp "snippet")
+             :url (get resp "url")
+             :fields [{:name "URL" :value (get resp "url")}]
+             :thumbnail {:url "https://1000logos.net/wp-content/uploads/2017/12/bing-emblem.jpg"}})})
 
 (defmethod invoke-command "w" [command state]
-  (let [q (str/join " " (:args command))
-        resp (wx/get-weather q state)]
+  (let [resp (wx/get-weather (str/join " " (:args command)) state)]
     {:type :embed :value resp}))
 
 (defmethod invoke-command "markov" [command state]
-    {:type :content :value (markov/markov (-> command
-                                              :args
-                                              first)
-                                          state)})
+    {:type :content
+     :value (markov/markov (-> command
+                               :args
+                               first)
+                           state)})
 
 (defmethod invoke-command :default [command state]
   {:type :content
-  :value (let [aka (:command command)]
-           (if-let [response (storage/get-aka aka state)]
-             response
-             nil))})
+   :value (if-let [response (storage/get-aka (:command command) state)]
+            response
+            nil)})
