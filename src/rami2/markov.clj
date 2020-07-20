37fedda54032a0388a6f16b9165e3f5f43c37be7
Joe Peralta <joe@crimsonhexagon.us>
2020-07-20 01:01:47 -0400

    refactored

--- src/rami2/markov.clj
+++ src/rami2/markov.clj
@@ -3,45 +3,30 @@
             [clojure.data.json :as json]
             [rami2.command :as command]))
 
-(defn markov
-  [prefix state]
-  (if (= prefix "communism")
-    "communism begins"
-    (let [resp (aws/invoke (aws/client {:api :lambda})
-                           {:op :Invoke
-                            :request {:FunctionName "markov"
-                                      :Payload (format "{ \"prefix\": \"%s\" }"
-                                                       prefix)}})]
-      (-> resp
-          :Payload
-          slurp
-          json/read-str
-          (get "body")
-          org.apache.commons.lang3.StringEscapeUtils/unescapeJava
-          (.replaceAll "\"" "")))))
+(defn format-request [prefix]
+  {:op :Invoke
+   :request {:FunctionName "markov"
+             :Payload (json/write-str {:prefix prefix})}})
+
+(defn transform-response [resp]
+  (-> resp
+      :Payload
+      slurp
+      json/read-str
+      (get "body")
+      org.apache.commons.lang3.StringEscapeUtils/unescapeJava
+      (.replaceAll "\"" "")))
+
+(defn query-lambda [prefix]
+  (aws/invoke
+    (aws/client {:api :lambda})
+    (format-request prefix)))
 
 (defmethod command/invoke-command "markov" [cmd state]
-  {:type :content
-    :value (markov (-> cmd
-                       :args
-                       first)
-                   state)})
-
-
-; (def resp (aws/invoke (aws/client {:api :lambda})
-;                       {:op :Invoke
-;                        :request {:FunctionName "markov"
-;                        :Payload "{ \"prefix\": \"🙂\" }"}}))
-; (get (json/read-str
-;       "{\"statusCode\": 200, \"body\": \"\\\"\\\\ud83d\\\\ude42\\\"\"}")
-;      "body")
-
-; (load-string (.replaceAll "\"\\ud83d\\ude42\"" "\"" ""))
-
-; (java.net.URLDecoder/decode "\\ud83d\\ude42" "UTF-8")
-
-; (org.apache.commons.lang3.StringEscapeUtils/unescapeJava "\\ud83d\\ude42")
-
-; (clojure.string/replace 
-;  "\\ud83d\\ude42"
-;  #"\\u([a-z0-9].)" "\\u$1")
+  (let [prefix (first (:args cmd))
+        response (if (= prefix "communism")
+                   "communism begins"
+                   (transform-response
+                    (query-lambda prefix)))]
+    {:type :content
+     :value response}))
